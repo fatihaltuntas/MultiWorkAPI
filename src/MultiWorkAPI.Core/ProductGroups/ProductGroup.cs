b62e380c656c6d2991786c135c78bb2ea74c5986
Fatih Altuntaş <36915172+fatihaltuntas@users.noreply.github.com>
2021-09-25 17:27:22 +0300

    product group required added

--- src/MultiWorkAPI.Core/ProductGroups/ProductGroup.cs
+++ src/MultiWorkAPI.Core/ProductGroups/ProductGroup.cs
@@ -3,6 +3,7 @@ using Abp.Domain.Entities.Auditing;
 using Abp.Timing;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -13,10 +14,13 @@ namespace MultiWorkAPI.ProductGroups
     [Table("ProductGroup")]
     public class ProductGroup: Entity<long>,IHasCreationTime
     {
+        [Required]
         public string Title { get; set; }
         public DateTime CreationTime { get; set; }
         public ProductGroupStatus Status { get; set; }
+        [Required]
         public long CreatedUserId { get; set; }
+        [Required]
         public long EditedUserId { get; set; }
 
         public ProductGroup()
--- src/MultiWorkAPI.Core/ProductGroups/ProductGroupBrand.cs
+++ src/MultiWorkAPI.Core/ProductGroups/ProductGroupBrand.cs
@@ -3,6 +3,7 @@ using Abp.Domain.Entities.Auditing;
 using MultiWorkAPI.Brands;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -13,8 +14,11 @@ namespace MultiWorkAPI.ProductGroups
     [Table("ProductGroupBrand")]
     public class ProductGroupBrand:Entity<long>,IHasCreationTime
     {
+        [Required]
         public long ProductGroupId { get; set; }
+        [Required]
         public long BrandId { get; set; }
+        [Required]
         public long CreatedUserId { get; set; }
         public long EditedUserId { get; set; }
         public DateTime CreationTime { get; set; }
